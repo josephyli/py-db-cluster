8944c9378c9ce4a63b198f208ad80189fccca773
josephyli <josephyli@gmail.com>
2017-01-30 23:30:06 -1000

    update_catalog() expanded to connect to database and execute collected statement. list_of_tables renamed table_list.

--- runDDL.py
+++ runDDL.py
@@ -52,22 +52,45 @@ def get_node_config(configfilename):
 
 # stores metadata about the DDL in a catalog database
 # using a list of tables that need to be created in the catalog
-def update_catalog(config_dict, list_of_tables):
+def update_catalog(config_dict, table_list):
+	cat_hn = config_dict['catalog.hostname']
+	cat_usr = config_dict['catalog.username']
+	cat_pw = config_dict['catalog.passwd']
+	cat_d = config_dict['catalog.driver']
 
-	sql = "CREATE TABLE dtables (tname char(32), nodedriver char(64), nodeurl char(128), nodeuser char(16), nodepasswd char(16), partmtd int, nodeid int, partcol char(32), partparam1 char(32), partparam2 char(32));\n"
+	sql = ["CREATE TABLE dtables (tname char(32), nodedriver char(64), nodeurl char(128), nodeuser char(16), nodepasswd char(16), partmtd int, nodeid int, partcol char(32), partparam1 char(32), partparam2 char(32));"]
 
-	# inserts into catalog the tables created for each node
-	for table in list_of_tables:
+	# prepares the sql statement to insert into catalog the tables in each node
+	for table in table_list:
 		for i in range(config_dict["catalog.numnodes"]):
-			try:
 				hn = config_dict['node'+str(i + 1)+'.hostname']
 				usr = config_dict['node'+str(i + 1)+'.username']
 				pw = config_dict['node'+str(i + 1)+'.passwd']
 				d = config_dict['node'+str(i + 1)+'.driver']
 
-				sql += "INSERT INTO dtables VALUES (\'%s\', \'%s\', \'%s\', \'%s\',\'%s\', NULL,%d,NULL,NULL,NULL);" % (table,d,hn,usr,pw,i+1)
-			except:
-				print "update_catalog error"
+				sql.append("INSERT INTO dtables VALUES (\'%s\', \'%s\', \'%s\', \'%s\',\'%s\', NULL,%d,NULL,NULL,NULL);" % (table,d,hn,usr,pw,i+1))
+	# connect and execute the sql statement
+	try:
+		connection = pymysql.connect(host=cat_hn,
+					user=cat_usr,
+					password=cat_pw,
+					db=cat_d,
+					charset='utf8mb4',
+					cursorclass=pymysql.cursors.DictCursor)
+		with connection.cursor() as cursor:
+			# execute every sql command
+			for command in sql:
+				try:
+					cursor.execute(command.strip() + ';')
+				except OperationalError, msg:
+					print "Command skipped: ", msg
+					connection.commit()
+
+	except:
+			print "couldn't connect to catalog"
+
+	
+
 	print sql
 
 
@@ -157,16 +180,16 @@ def main():
 	sql_commands = read_DDL(args.ddlfile)
 
 	# list of tables is used to update catalog with metadata
-	list_of_tables = []
+	table_list = []
 	for command in sql_commands:
 		if command.split()[0].upper() == "CREATE":
-			list_of_tables.append((re.split('\s|\(',command)[2]))
+			table_list.append((re.split('\s|\(',command)[2]))
 	print "list of tables needed:" 
-	print list_of_tables
+	print table_list
 	print "resulting sql commands"
 	print sql_commands
 
-	update_catalog(nodes_dict,list_of_tables)
+	update_catalog(nodes_dict,table_list)
 	print "-" * 80
 	print
 
