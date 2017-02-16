3bc75b7f77224a53c3f238b6fc0a9e2e1c6f5a60
Joseph Li <josephyli@gmail.com>
2017-02-15 22:07:56 -1000

    runSQL.py updated to include new runDDL connection implementation. Initial loadCSV program commit

--- runSQL.py
+++ runSQL.py
@@ -3,6 +3,7 @@ import sys
 import re
 import os
 import pymysql.cursors
+from pymysql import OperationalError
 from ConfigParser import SafeConfigParser
 from StringIO import StringIO
 import csv
@@ -82,7 +83,7 @@ def update_catalog(config_dict, table_list):
 	cat_dr = config_dict['catalog.driver']
 	cat_db = config_dict['catalog.database']
 
-	sql = ["DROP TABLE IF EXISTS dtables; CREATE TABLE dtables (tname char(32), nodedriver char(64), nodeurl char(128), nodeuser char(16), nodepasswd char(16), partmtd int, nodeid int, partcol char(32), partparam1 char(32), partparam2 char(32));"]
+	sql = ["DROP TABLE dtables", "CREATE TABLE dtables (tname char(32), nodedriver char(64), nodeurl char(128), nodeuser char(16), nodepasswd char(16), partmtd int, nodeid int, partcol char(32), partparam1 char(32), partparam2 char(32));"]
 
 	# prepares the sql statement to insert into catalog the tables in each node
 	for table in table_list:
@@ -93,27 +94,31 @@ def update_catalog(config_dict, table_list):
 				dr = config_dict['node'+str(i + 1)+'.driver']
 
 				sql.append("INSERT INTO dtables VALUES (\'%s\', \'%s\', \'%s\', \'%s\',\'%s\', NULL,%d,NULL,NULL,NULL);" % (table,dr,hn,usr,pw,i+1))
-	# connect and execute the sql statement
 	try:
+		# connect and execute the sql statement
 		connection = pymysql.connect(host=cat_hn,
 					user=cat_usr,
 					password=cat_pw,
 					db=cat_db,
 					charset='utf8mb4',
 					cursorclass=pymysql.cursors.DictCursor)
+
+		print "[SUCCESSFUL CATALOG CONNECTION] <"+connection.host+" - "+connection.db+">", connection
+		print
+
 		with connection.cursor() as cursor:
 			# execute every sql command
 			for command in sql:
 				try:
-					print connection, "updating catalog: executing ", command
+					print command
 					print
 					cursor.execute(command.strip() + ';')
+					connection.commit()
 				except OperationalError, msg:
 					print "Command skipped: ", msg
-					connection.commit()
-
-	except:
-			print "couldn't connect to catalog"
+	except pymysql.err.InternalError as d:
+		print "[FAILED TO UPDATE CATALOG]"
+		print d
 
 # returns a list of connections to all nodes
 def get_connections(config_dict):
@@ -150,10 +155,10 @@ def run_commmands_against_nodes(connections, sql_commands):
 	# create a list of jobs
 	list_of_threads = []
 	for connection in connections:
-			for command in sql_commands:
-				print "[JOB CREATED]<",connection.host+"> for db <",connection.db+">"
-				list_of_threads.append(Thread(target=run_sql_command_against_node, args=(connection, command)))
-	print
+			print "[JOB CREATED] <"+ connection.host+ " - " + connection.db+ ">"
+			print connection
+			list_of_threads.append(Thread(target=run_sql_commands_against_node, args=(connection, sql_commands)))
+			print
 	# start up all jobs
 	for t in list_of_threads:
 		t.start()
@@ -162,19 +167,16 @@ def run_commmands_against_nodes(connections, sql_commands):
 		time.sleep(1)
 
 
-def run_sql_command_against_node(connection, sql_command):
+def run_sql_commands_against_node(connection, sql_commands):
 	with connection.cursor() as cursor:
-		print "-    -    " * 8
-		print "[", connection.host, "]"
-		print sql_command
 		try:
-			cursor.execute(sql_command.strip() + ';')
+			for c in sql_commands:
+				cursor.execute(c.strip() + ';')
 			connection.commit()
-			print "Command successful"
+			print "[JOB SUCCESSFUL] <"+connection.host+ " - " + connection.db+ ">"
+			connection.close()
 		except pymysql.MySQLError as e:
-			print 'Got error {!r}, errno is {}'.format(e, e.args[0])
-		print "-    -    " * 8
-		print
+			print "[JOB FAILED] <"+connection.host+ " - " + connection.db+ "> ERROR: {!r}, ERROR NUMBER: {}".format(e, e.args[0])
 
 def print_pretty_dict(idict):
 	import json
@@ -191,54 +193,67 @@ def main():
 	print
 
 	# read configuration and return a dictionary -------------------------------
-	# print "parsing", args.configfile, "into a dict..."
+	temp = "PARSING " + str(args.configfile) + "..."
+	print
+	print temp.center(80, " ")
 	nodes_dict = get_node_config(args.configfile)
-	# print_pretty_dict(nodes_dict)
-	# print
-	# print "-" * 80
-	# print
+	print_pretty_dict(nodes_dict)
+	print
+	print "-" * 80
+	print
 
 	# return a list of connections to all nodes --------------------------------
-	print "Creating Connections..."
+	print "CREATING CONNECTIONS...".center(80, " ")
+	print
 	node_connections = get_connections(nodes_dict)
 	# if no connections were made, terminate the program, comment this out for testing
 	if len(node_connections) == 0:
 		print "Terminating due to connection failures..."
 		sys.exit()
-	print "Number of Connections:", str(len(node_connections))
+	print "# of connections:", str(len(node_connections))
+	print
 	for c in node_connections:
-		print c.host
+		print "HOST: " + c.host + " DB: " + c.db + " " + str(c)
 	print
 	print "-" * 80
 	print
 
 	# read DDL and return a list of sql commands -------------------------------
-	print "parsing ", args.ddlfile, "into sql commands..."
+	print "PARSING SQL COMMANDS...".center(80, " ")
+	print
 	sql_commands = read_DDL(args.ddlfile)
 	# list of tables is used to update catalog with metadata
 	table_list = []
 	for command in sql_commands:
 		if command.split()[0].upper() == "CREATE":
 			table_list.append((re.split('\s|\(',command)[2]))
-	# print "list of tables needed:"
-	# print table_list
-	# print "resulting sql commands"
-	# print sql_commands
+	print "[SQL COMMANDS]:"
+	for s in sql_commands:
+		print s.strip()
+	print
+	print "TABLES:"
+	print table_list
+	print
+	print "-" * 80
+	print
+
+	# update catalog  ----------------------------------------------------------
+	print "UPDATING CATALOG...".center(80, " ")
+	print
 	update_catalog(nodes_dict,table_list)
 	print
 	print "-" * 80
 	print
 
 	# run the commands against the nodes ---------------------------------------
-	print "creating tables in nodes according to ddlfile..."
+	print "EXECUTING DDL COMMANDS ON NODES...".center(80, " ")
 	print
 	run_commmands_against_nodes(node_connections, sql_commands)
-
-	# run the commands against the nodes ---------------------------------------
-	print "running sql commands against all connections..."
-	print
+	# reconnect quietly
+	node_connections = get_connections(nodes_dict)
 	sql_commands = read_SQL(args.sqlfile)
 	run_commmands_against_nodes(node_connections, sql_commands)
+
 
 	print
 	print "=" * 80
