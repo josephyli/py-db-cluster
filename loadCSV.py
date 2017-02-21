805747bedb919afef72e45d5da2ec73d9ef323e0
Tyler Nakamura <tyler.c.nakamura@gmail.com>
2017-02-20 18:18:53 -1000

    add support for hash partitions

--- loadCSV.py
+++ loadCSV.py
@@ -2,7 +2,7 @@ import argparse
 import csv
 import os
 import pymysql.cursors
-import re 
+import re
 import sys
 
 from ConfigParser import SafeConfigParser
@@ -55,7 +55,7 @@ def get_partition_config(configfilename):
 			if cp.has_option('fakesection', 'numnodes'):
 				numnodes = cp.getint('fakesection', 'numnodes')
 				config_dict['catalog.numnodes'] = numnodes
-			
+
 				# read depending on partition method
 				if (partition_method_string == 'notpartition'):
 					for node in range(1, numnodes + 1):
@@ -80,9 +80,12 @@ def get_partition_config(configfilename):
 								config_dict["partition.node" + str(node) + "." + parameter] = cp.getint('fakesection', "partition.node" + str(node) + "." + parameter)
 					return config_dict
 
-
 			elif (cp.get('fakesection', 'partition.method') == 'hash'):
-				print "Hash method selected!!!"
+				print "Loading the CSV based hash partitioning"
+				config_dict['catalog.numnodes'] = cp.getint('fakesection', 'partition.param1')
+				config_dict['partition.method'] = "hash"
+				config_dict['partition.column'] = cp.get('fakesection', 'partition.column')
+				config_dict['partition.param1'] = cp.get('fakesection', 'partition.param1')
 				return config_dict
 	else:
 		print("No config file found at", configfilename)
@@ -124,24 +127,36 @@ def update_catalog_with_partitions(config_dict):
 	cat_pw = config_dict['catalog.passwd']
 	cat_dr = config_dict['catalog.driver']
 	cat_db = config_dict['catalog.database']
-	
+
 	tablename = config_dict['catalog.tablename']
 	pm = config_dict['catalog.partition.method']
 	pc = config_dict['catalog.partition.column']
 
+	try:
+		number_of_nodes = config_dict["catalog.numnodes"]
+	except KeyError as e:
+		print "The number of nodes was not specified in the config file..."
+
+	print "THE NUMBER OF NODES IS",  number_of_nodes
+
 	sql = []
 
-	# prepares the sql statement to insert into catalog the tables in each node
-	for nodeid in range(1, config_dict["catalog.numnodes"] + 1):
+	# if using a hashing partition method, the table is updated differently
+	if pm == 2:
+		p1 = config_dict['partition.param1']
+		for i in range(number_of_nodes):
+			sql.append("UPDATE dtables SET partmtd = 2, partcol = \'%s\', partparam1 = %d, partparam2 = \'NULL\' WHERE tname=\'%s\' AND nodeid = %d; " % (pc, int(p1), tablename, int(i) + 1))
+		print sql
+	else:
+		# prepares the sql statement to insert into catalog the tables in each node for range
+		for nodeid in range(1, number_of_nodes + 1):
 			# hn = config_dict['node'+str(i + 1)+'.hostname']
 			# usr = config_dict['node'+str(i + 1)+'.username']
 			# pw = config_dict['node'+str(i + 1)+'.passwd']
 			# dr = config_dict['node'+str(i + 1)+'.driver']
 			p1 = config_dict['partition.node'+str(nodeid)+'.param1']
 			p2 = config_dict['partition.node'+str(nodeid)+'.param2']
-
 			sql.append("UPDATE dtables SET partmtd = %d, partcol = \'%s\', partparam1 = %d, partparam2 = %d WHERE tname=\'%s\' AND nodeid = %d; " % (pm, pc, p1, p2, tablename, nodeid))
-
 	try:
 		# connect and execute the sql statement
 		connection = pymysql.connect(host=cat_hn,
@@ -158,8 +173,6 @@ def update_catalog_with_partitions(config_dict):
 			# execute every sql command
 			for command in sql:
 				try:
-					# print command
-					# print
 					cursor.execute(command.strip() + ';')
 					connection.commit()
 				except OperationalError, msg:
@@ -206,19 +219,62 @@ def update_catalog_with_partitions(config_dict):
 			config_dict['node'+str(nodeid)+'.partparam1'] = entry['partparam1']
 			config_dict['node'+str(nodeid)+'.partparam2'] = entry['partparam2']
 			config_dict['node'+str(nodeid)+'.driver'] = entry['nodedriver']
-			config_dict['node'+str(nodeid)+'.username'] = entry['nodeuser'] 
+			config_dict['node'+str(nodeid)+'.username'] = entry['nodeuser']
 			config_dict['node'+str(nodeid)+'.tname'] = entry['tname']
 			config_dict['node'+str(nodeid)+'.passwd'] = entry['nodepasswd']
 			config_dict['node'+str(nodeid)+'.database'] = entry['nodeurl'].rsplit('/', 1)[-1]
-	else: 
+	else:
 		config_dict['catalog.numnodes'] = 0
 	return config_dict
+
+def hash_insert(csv_list, node_connections, config_dict):
+	res = ""
+	columns = []
+
+	# identifying the partition column
+	for connection in node_connections:
+		with connection.cursor() as cursor:
+			try:
+				cursor.execute("SHOW COLUMNS IN " +config_dict['catalog.tablename'].upper() + ";")
+				res = cursor.fetchall()
+				connection.commit()
+			except pymysql.MySQLError as e:
+				print e
+	for d in res:
+		columns.append(d['Field'])
+
+	partition_index = 0
+	for f in columns:
+		if f.lower() == config_dict['partition.column'].lower():
+			break
+		partition_index += 1
+
+	print
+	print "Index",partition_index,"of",columns,"is the partition column"
+	print
+
+	for i in range(len(csv_list)):
+		nodeid = int(csv_list[i][partition_index]) % int(config_dict['catalog.numnodes'])
+		with node_connections[nodeid].cursor() as cursor:
+			sql_statement = "INSERT INTO %s VALUES (\'%s\',\'%s\',\'%s\') ;" % (config_dict['catalog.tablename'].upper(), csv_list[i][0], csv_list[i][1], csv_list[i][2])
+			print sql_statement
+			cursor.execute(sql_statement)
+			res = cursor.fetchone()
+			node_connections[nodeid].commit()
+			print res
+			#cursor.close()
+
+
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("configfile", help="Location of Config File, See the README for more information")
 	parser.add_argument("csvfile", help="Location of CSV File, See the README for more information")
 	args = parser.parse_args()
+
+	if args.csvfile[-4:].lower() != ".csv":
+		print "Are you certain you are providing a CSV file?"
+		sys.exit(1)
 
 	# read the config file into a dictionary
 	print "READING CONFIG FILE...".center(80, " ")
@@ -235,7 +291,6 @@ def main():
 	print
 	print "-" * 80
 	print
-	
 
 	# return a list of connections to all nodes
 	print "CREATING CONNECTIONS...".center(80, " ")
@@ -255,7 +310,7 @@ def main():
 	tablename = "NotReal"
 	csv_list = loadCSV(args.configfile, args.csvfile)
 	sql = "INSERT INTO %s VALUES(%s,%s,%s);", (tablename, csv_list[0][0], csv_list[0][1], csv_list[0][2],)
-	print sql
+
 
 	# handle different types of partitioning methods
 	if config_dict['catalog.partition.method'] == 0: # if no partitioning scheme is set - csv is propagated to all nodes
@@ -263,8 +318,8 @@ def main():
 	elif config_dict['catalog.partition.method'] == 1: # if range partitioning is set
 		pass
 	elif config_dict['catalog.partition.method'] == 2: # if hash partitioning is set
-		print "Inserting into the tables based on hash"
-		hash_insert(csv_list, node_connections)
+		print "Using Hash Partitioning as Partitioning Method..."
+		hash_insert(csv_list, node_connections, config_dict)
 
 	#update the catalog using the stored table name in the node_dict
 	# print nodes_dict['catalog.hostname']
